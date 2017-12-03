a0aa0dfde1c202a48d873760503caad624971a66
Sami Jawhar <sami.n.jawhar@gmail.com>
2017-12-02 18:36:33 -0800

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM alpine:3.6
+RUN apk add --no-cache socat
+
+ENV DOCKER_SOCKET=/var/run/docker.sock \
+	LISTEN_PORT=2376 \
+	LISTEN_SECURE=true \
+	CERTS_DIR=/run/secrets \
+	SERVER_CERT=server-cert.pem \
+	SERVER_KEY=server-key.pem \
+	CA_FILE=ca.pem
+
+EXPOSE $LISTEN_PORT
+
+COPY run.sh /
+
+ENTRYPOINT ["/run.sh"]
+CMD ["-dd"]
---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ "$LISTEN_SECURE" == "false" ]; then
+	socat "$@" TCP-LISTEN:"$LISTEN_PORT",reuseaddr,fork UNIX-CLIENT:"$DOCKER_SOCKET"
+else
+	socat "$@" \
+		OPENSSL-LISTEN:"$LISTEN_PORT",reuseaddr,fork,certificate="$CERTS_DIR/$SERVER_CERT",key="$CERTS_DIR/$SERVER_KEY",cafile="$CERTS_DIR/$CA_FILE" \
+		UNIX-CLIENT:"$DOCKER_SOCKET"
+fi
